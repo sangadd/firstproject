96b194e783f9eea37f622b3d610178b27f92bf4f
sangadd <baesanga1207@gmail.com>
2025-02-01 22:22:08 +0900

    [15장] 댓글 컨트롤러와 서비스 만들기

--- src/main/java/com/example/firstproject/api/CommentApiController.java
+++ src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -5,9 +5,7 @@ import com.example.firstproject.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,20 +17,56 @@ public class CommentApiController {
 
     // 1. 댓글 조회
     @GetMapping("/api/articles/{articleId}/comments")
-    public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId){
+    public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId) {
         // 왜 반환형이 List<Comment>가 아니고 ResponseEntity<List<CommentDto>> 일까? 
         // 그 이유는 DB에서 조회한 댓글 엔티티 목록은 List<Comment> 지만 엔티티를 DTO로 변환하면 List<CommentDto> 가 되기 떄문이다. 
         // 그리고 응답 코드를 같이 보내려면 ResponseEntity 클래스를 활용해야해서 최종 반환형이 되는 것임 
-        
+
         // 서비스에 위임 
         List<CommentDto> dtos = commentService.comments(articleId);
         // articleId를 전달값으로 넘긴 이유는 해당 게시글의 id를 알아야 해당 게시글의 댓글을 가져올 수 있기 떄문.
-        
+
         // 결과 응답 
         return ResponseEntity.status(HttpStatus.OK).body(dtos);
         // return (반환값이 있다면) ? good : bad; -> 삼항연산자
         // 그러나 실제 개발에서는 삼항연산자보다는 예외 처리 방식을 선호함 
         // 예외처리란 예기지 못한 상황이 발생했을 때를 대비해 대처하는 코드를 미리 작성하는 것을 뜻함 
         // REST API의 응답은 ResponseEntity에 실어 보내기 떄문에 ResponseEntity의 상태(status)에는 OK, 본문(body)에는 조회한 댓글 목록인 dtos를 실어 보냄
+    }
+
+    // 2. 댓글 생성
+    @GetMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<CommentDto> create(@PathVariable Long articleId,
+                                             @RequestBody CommentDto dto) {
+        // 서비스에 위임
+        CommentDto createDto = commentService.create(articleId, dto);
+
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(createDto); // 상태(status)에는 OK, 본문(body)에는 생성한 댓글 데이터인 createDto 실어보냄
+    }
+    /*
+    * @RequestBody
+    * 이 어노테이션은 HTTP 요청 본문에 실린 내용 (JSON, XML, YAML)을 자바 객체로 변환해줌
+    */
+
+    // 3. 댓글 수정
+    @PostMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> update(@PathVariable Long id,
+                                             @RequestBody CommentDto dto) {
+        // 서비스에 위임
+        CommentDto updatedDto = commentService.update(id, dto);
+
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(updatedDto); // 상태(status)에는 OK, 본문(body)에는 생성한 댓글 데이터인 createDto 실어보냄
+    }
+
+    // 4. 댓글 삭제
+    @DeleteMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> delete(@PathVariable Long id) {
+        // 서비스에 위임
+        CommentDto deletedDto = commentService.delete(id);
+
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(deletedDto); // 상태(status)에는 OK, 본문(body)에는 생성한 댓글 데이터인 createDto 실어보냄
     }
 }
--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -1,11 +1,13 @@
 package com.example.firstproject.service;
 
 import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.entity.Article;
 import com.example.firstproject.entity.Comment;
 import com.example.firstproject.repository.ArticleRepository;
 import com.example.firstproject.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,12 +48,62 @@ public class CommentService {
                 .collect(Collectors.toList()); // 스트림 데이터를 리스트 자료형으로 변환
 
         /*
-        * 스트림의 특징
-        * 스트림은 자바의 컬렉션, 즉 리스트와 해시맵 등의 데이터 묶음을 요소별로 순차적으로 조작하는 데 좋다.
-        *
-        * 1. 원본 데이터를 읽기만 하고 변경하지 않는다.
-        * 2. 정렬된 결과를 컬렉션이나 배열에 담아 반환할 수 있다.
-        * 3. 내부 반복문으로 반복문이 코드상에 노출되지 않는다.
-        */
+         * 스트림의 특징
+         * 스트림은 자바의 컬렉션, 즉 리스트와 해시맵 등의 데이터 묶음을 요소별로 순차적으로 조작하는 데 좋다.
+         *
+         * 1. 원본 데이터를 읽기만 하고 변경하지 않는다.
+         * 2. 정렬된 결과를 컬렉션이나 배열에 담아 반환할 수 있다.
+         * 3. 내부 반복문으로 반복문이 코드상에 노출되지 않는다.
+         */
+    }
+
+    @Transactional
+    public CommentDto create(Long articleId, CommentDto dto) {
+        // 이 메서드는 DB 내용을 바꾸기 떄문에 실패할 경우를 대비해야해서 @Transactional을 추가해 문제가 발생했을 때 롤백하게 해줌
+        // 1. DB에서 부모 게시글을 조회해 가져오고 없을 경우 예외 발생시키기
+        Article article = articleRepository.findById(articleId) // (1) 부모 게시글 가져오기
+                .orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패!" + "대상 게시글이 없습니다.")); // (2) 없으면 에러 메시지 출력
+
+        // 2. 부모 게시글의 새 댓글 엔티티 생성
+        Comment comment = Comment.createComment(dto, article);
+
+        // 3. 생성된 엔티티를 DB에 저장
+        Comment created = commentRepository.save(comment);
+
+        // 4. DB에 저장한 엔티티를 DTO로 변환해 반환하기
+        return CommentDto.createCommentDto(created);
+    }
+
+    /*
+    * orElseThrow 메서드
+    * Optional 객체(존재할 수도 있지만 안 할 수도 있는 객체, 즉 null이 될 수도 있는 개테에 값이 존재하면 그 값을 반환하고 존재하지 않으면 전달값으로 보낸 예외를 발생시키는 메서드
+    전달값으로 IllegalArgumentException 쿨래스를 사용하면 매소드가 잘못됐거나 부적합한 전달값을 보냈음을 나타냄
+    */
+    @Transactional
+    public CommentDto update(Long id, CommentDto dto) {
+        // 1. DB에서 해당 게시글을 조회해 가져오고 없을 경우 예외 발생시키기
+        Comment target = commentRepository.findById(id) // (1) 수정할 댓글 가져오기 
+                .orElseThrow(() -> new IllegalArgumentException("댓글 수정 실패!" + "대상 댓글이 없습니다.")); // (2) 없으면 에러 메시지 출력
+
+        // 2. 가져온 댓글 내용 수정하기
+        target.patch(dto);
+
+        // 3. 수정한 댓글을 DB에 갱신하기 (수정 데이터로 덮어쓰기)
+        Comment updated = commentRepository.save(target);
+
+        // 4. DB에 반영된 엔티티를 DTO로 변환해 컨트롤러오 반환하기
+        return CommentDto.createCommentDto(updated);
+    }
+
+    public CommentDto delete(Long id) {
+        // 1. DB에서 해당 게시글을 조회해 가져오고 없을 경우 예외 발생시키기
+        Comment target = commentRepository.findById(id) // (1) 삭제할 댓글 가져오기
+                .orElseThrow(() -> new IllegalArgumentException("댓글 삭제 실패!" + "대상이 없습니다.")); // (2) 없으면 에러 메시지 출력
+
+        // 2. 가져온 댓글을 DB에서 삭제하기
+        commentRepository.delete(target);
+
+        // 3. 삭제한 댓글을 DTO로 변환해 반환하기 
+        return CommentDto.createCommentDto(target);
     }
 }
